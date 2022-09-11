603d0c71d180719f7dfe8504ccc10b6bba3acf7a
Philipp Hack <philipp.hack@gmail.com>
2022-09-11 11:04:38 +0200

    Search and retrieve messages

--- evenless_api/main.py
+++ evenless_api/main.py
@@ -1,8 +1,49 @@
-from fastapi import FastAPI
+import email
+import email.parser
+import email.policy
+import os
+from email.message import EmailMessage
+from typing import Generator, List
+
+import notmuch as nm
+from fastapi import Depends, FastAPI
+
+from .models import Config, Message, MessageSummary, Query
 
 app = FastAPI(title="EvenLess")
 
 
-@app.get("/")
-def get_root() -> str:
-    return "Hello World"
+def get_db() -> Generator[nm.Database, None, None]:
+    with nm.Database(os.environ["EVENLESS_DB_PATH"]) as db:
+        yield db
+
+
+@app.get("/", response_model=Config)
+def get_root(db: nm.Database = Depends(get_db)) -> Config:
+    """Get configuration information"""
+    return Config(database_path=db.get_path(), database_version=db.get_version())
+
+
+@app.get("/tags/")
+def get_tags(db: nm.Database = Depends(get_db)) -> List[str]:
+    """Get a list of all tags"""
+    return [t for t in db.get_all_tags()]
+
+
+@app.post("/search/messages", response_model=List[MessageSummary])
+def search_messages(
+    query: Query, db: nm.Database = Depends(get_db)
+) -> List[MessageSummary]:
+    """Query the database and return summaries of all matching messages"""
+    q = query.prepare(db)
+    messages = q.search_messages()
+    return [MessageSummary.from_message(m) for m in messages]
+
+
+@app.post("/message/", response_model=Message)
+def get_message(id: str, db: nm.Database = Depends(get_db)) -> Message:
+    return Message.from_message(db.find_message(id))
+    # with open(db.find_message(id).get_filename(), "r") as fp:
+    #     message: EmailMessage = email.message_from_file(
+    #         fp, _class=EmailMessage, policy=email.policy.default
+    #     )
---
+++ evenless_api/models/__init__.py
@@ -0,0 +1,91 @@
+import email.policy
+from datetime import datetime
+from email import message_from_file
+from email.message import EmailMessage
+from enum import Enum
+from typing import List, cast
+
+import notmuch as nm
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    """Configuration information"""
+
+    database_path: str
+    database_version: int
+
+
+class MessageSummary(BaseModel):
+    """A Summary of an email message"""
+
+    id: str
+    date: datetime
+    sender: str
+    recipient: str
+    subject: str
+    tags: List[str]
+
+    @classmethod
+    def from_message(cls, message: nm.Message) -> "MessageSummary":
+        """Create a summary from a notmuch Message object"""
+        return cls(
+            id=message.get_message_id(),
+            date=message.get_date(),
+            sender=message.get_header("From"),
+            recipient=message.get_header("To"),
+            subject=message.get_header("Subject"),
+            tags=[t for t in message.get_tags()],
+        )
+
+
+class Message(MessageSummary):
+    """An email message"""
+
+    body: str
+
+    @classmethod
+    def from_message(cls, message: nm.Message) -> "Message":
+        with open(message.get_filename(), "r") as fp:
+            email_message = cast(
+                EmailMessage,
+                message_from_file(fp, _class=EmailMessage, policy=email.policy.default),
+            )
+            body = email_message.get_body()
+            if body:
+                return cls(
+                    id=message.get_message_id(),
+                    date=message.get_date(),
+                    sender=message.get_header("From"),
+                    recipient=message.get_header("To"),
+                    subject=message.get_header("Subject"),
+                    tags=[t for t in message.get_tags()],
+                    body=body.get_payload(),
+                )
+            else:
+                raise ValueError()
+
+
+class SortOrder(str, Enum):
+    """Sort order for search query"""
+
+    oldest_first = "OLDEST_FIRST"
+    newest_first = "NEWEST_FIRST"
+    message_id = "MESSAGE_ID"
+    unsorted = "UNSORTED"
+
+
+class Query(BaseModel):
+    """A search query"""
+
+    string: str = ""
+    order: SortOrder = SortOrder.newest_first
+    exclude_tags: List[str] = []
+
+    def prepare(self, db: nm.Database) -> nm.Query:
+        """Prepare an executable query to the given database"""
+        db_query = db.create_query(self.string)
+        for tag in self.exclude_tags:
+            db_query.exclude_tag(tag)
+        db_query.set_sort(db_query.SORT.__getattribute__(self.order.value))
+        return db_query
