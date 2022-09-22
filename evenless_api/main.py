c0e47553261a00db5ebd14aebeb44c069a2e5e52
Philipp Hack <philipp.hack@gmail.com>
2022-09-22 21:12:14 +0200

    refactor: async api

--- evenless_api/main.py
+++ evenless_api/main.py
@@ -12,19 +12,19 @@ app.add_middleware(GZipMiddleware)
 
 
 @app.get("/", response_model=Info)
-def get_root(db: nm.Database = Depends(dependencies.get_db)) -> Info:
+async def get_root(db: nm.Database = Depends(dependencies.get_db)) -> Info:
     """Get configuration information"""
     return Info(database_path=db.get_path(), database_version=db.get_version())
 
 
 @app.get("/tags/")
-def get_tags(db: nm.Database = Depends(dependencies.get_db)) -> list[str]:
+async def get_tags(db: nm.Database = Depends(dependencies.get_db)) -> list[str]:
     """Get a list of all tags"""
     return [t for t in db.get_all_tags()]
 
 
 @app.post("/search/messages/", response_model=list[MessageSummary])
-def search_messages(
+async def search_messages(
     query: Query, db: nm.Database = Depends(dependencies.get_db)
 ) -> list[MessageSummary]:
     """Query the database and return summaries of all matching messages"""
@@ -37,7 +37,7 @@ def search_messages(
 
 
 @app.post("/message/", response_model=Message)
-def get_message(
+async def get_message(
     message_id: str,
     db: nm.Database = Depends(dependencies.get_db),
     get_message_body=Depends(dependencies.get_message_body),
